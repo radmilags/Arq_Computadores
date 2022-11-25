f6ee36c459bd8b574ec9ffbc118e76aaf418c7c9
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2022-11-25 07:56:01 -0300

    teste

--- montador/linhas.py
+++
@@ -1,9 +0,0 @@
-def contar(linha, instrucoes):
-  dois_pontos = linha.count(':')
-  instrucao = 0
-  if dois_pontos > 0:
-    dois_pontos = linha.index(':')
-    instrucao = linha[:dois_pontos]
-    linha = linha[dois_pontos + 1:] + " "
-  instrucoes.append(instrucao)
-  return linha, instrucoes
