d3898577325ac3e906d4a430c71eed1661f7f1bf
Radmila Gama <49914278+radmilags@users.noreply.github.com>
2022-11-25 09:31:45 -0300

    Delete Projeto directory

--- Projeto/decbin.py
+++
@@ -1,8 +0,0 @@
-def decbin(n):
-  ans = ""
-  while n//2>=1:
-      ans += str(n%2)
-      if n//2 == 1: ans += str(n//2)
-      n = n//2
-  ans = ans[::-1]
-  return ans
--- Projeto/main.py
+++
@@ -1,6 +0,0 @@
-from binhex import bintohexa
-from decbin import decbin
-n = input()
-m = int(input())
-print(bintohexa(n))
-print(decbin(m))
